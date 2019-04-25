c47dfc29d9f3028f616883e6ee1c381a97602138
Le Quang Minh <minhle.91719@gmail.com>
2019-04-25 22:19:01 +0700

    haha

---
+++ request.go
@@ -0,0 +1,130 @@
+package mrequest
+
+import (
+    "bytes"
+    "compress/gzip"
+    "context"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "time"
+)
+
+const (
+    DefaultMaxRequestPerSecond = 1
+)
+
+type RQ struct {
+    _host      string
+    _mapCookie map[string]http.Cookie
+    _ref       string
+    ua         string
+    client     *http.Client
+
+    requestAccess chan int
+
+    ctx    context.Context
+    cancel context.CancelFunc
+}
+
+type contentType string
+
+const (
+    WWWType    contentType = "application/x-www-form-urlencoded"
+    JSONType   contentType = "application/json"
+    TextJSType contentType = "text/javascript"
+)
+
+func NewRequest(host string, client *http.Client, rps int) IRequest {
+    if rps == 0 {
+        rps = DefaultMaxRequestPerSecond
+    }
+    rq := &RQ{
+        _host:         host,
+        _mapCookie:    make(map[string]http.Cookie),
+        _ref:          host,
+        ua:            randomUA(),
+        requestAccess: make(chan int, rps),
+        client:        client,
+    }
+    rq.ctx, rq.cancel = context.WithCancel(context.Background())
+    go rq.grantRequest(rps)
+    return rq
+}
+
+type IRequest interface {
+    Request(f func() (*http.Request, error)) ([]byte, error)
+    Close()
+}
+
+func (r *RQ) grantRequest(maxRequestPerSecond int) {
+    ticker := time.NewTicker(1 * time.Second)
+    for range ticker.C {
+        select {
+        case <-r.ctx.Done():
+            return
+        default:
+            for len(r.requestAccess) < maxRequestPerSecond {
+                r.requestAccess <- 1
+            }
+        }
+    }
+}
+func (r *RQ) Close() {
+        r.cancel()
+}
+func (r *RQ) Request(f func() (*http.Request, error)) ([]byte, error) {
+    <-r.requestAccess
+    var (
+        req *http.Request
+        res *http.Response
+
+        reader io.ReadCloser
+
+        err error
+    )
+    req, err = f()
+    if err != nil {
+        return nil, err
+    }
+    req.Header.Add("User-Agent", r.ua)
+    switch req.Method {
+    case http.MethodPost:
+        contentIO, err := req.GetBody()
+        if err != nil {
+            return nil, err
+        }
+        content, err := ioutil.ReadAll(contentIO)
+        ioutil.NopCloser(bytes.NewBuffer(content))
+        req.Header.Add("Content-Length", fmt.Sprintf("%d", len(string(content))))
+    }
+    req.Header.Add("Accept", "*/*")
+    req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+    req.Header.Add("Accept-Encoding", "gzip")
+    req.Header.Add("Referer", r._ref)
+    for _, v := range r._mapCookie {
+        ck := http.Cookie{}
+        ck = v
+        req.AddCookie(&ck)
+    }
+    if res, err = r.client.Do(req); err != nil {
+        return nil, err
+    }
+    listCookie := res.Cookies()
+    for _, v := range listCookie {
+        r._mapCookie[v.Name] = *v
+    }
+    switch res.Header.Get("Content-Encoding") {
+    case "gzip":
+        reader, err = gzip.NewReader(res.Body)
+    default:
+        reader = res.Body
+    }
+    defer func() {
+        res.Body.Close()
+        reader.Close()
+    }()
+    r._ref = req.URL.String()
+    return ioutil.ReadAll(reader)
+}
