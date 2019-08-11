1abedc797ad181bc4270769cdf6681a04c361b03
Le Quang Minh <minhle.91719@gmail.com>
2019-08-12 04:25:37 +0700

    fix logic

--- request.go
+++ request.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	
+	"errors"
 	"golang.org/x/time/rate"
 	"time"
 )
@@ -15,6 +16,7 @@ import (
 var (
 	defaultLimiter = rate.NewLimiter(rate.Every(5*time.Second), 1)
 )
+
 // TODO: add request file return io.ReadSeeker
 type RQ struct {
 	_host      string
@@ -58,15 +60,18 @@ func NewRequest(ctx context.Context, host string, client *http.Client, limiter *
 
 type IRequest interface {
 	Request(f func() (*http.Request, error)) ([]byte, error)
+	ExportCookie() []*http.Cookie
+	AddCookie(list []*http.Cookie)
 }
 
 func (r *RQ) Request(f func() (*http.Request, error)) ([]byte, error) {
 	for !r.limiter.Allow() {
 	}
-	if _, ok := <-r.ctx.Done(); ok {
+	select {
+	case <-r.ctx.Done():
 		return nil, errors.New("context canceled")
+	default:
 	}
-	
 	var (
 		req *http.Request
 		res *http.Response
@@ -92,23 +97,34 @@ func (r *RQ) Request(f func() (*http.Request, error)) ([]byte, error) {
 		content, err := ioutil.ReadAll(contentIO)
 		req.Header.Add("Content-Length", fmt.Sprintf("%d", len(string(content))))
 	}
-	req.Header.Add("Accept", "*/*")
-	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
-	req.Header.Add("Accept-Encoding", "gzip")
-	req.Header.Add("Referer", r._ref)
+	switch {
+	case req.Header.Get("Accept") == "":
+		req.Header.Add("Accept", "*/*")
+	case req.Header.Get("Accept-Language") == "":
+		req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+	case req.Header.Get("Accept-Encoding") == "":
+		req.Header.Add("Accept-Encoding", "gzip")
+	case req.Header.Get("Referer") == "":
+		req.Header.Add("Referer", r._ref)
+	}
+	
 	for _, v := range r._mapCookie {
 		ck := http.Cookie{}
 		ck = v
 		req.AddCookie(&ck)
-		//fmt.Println(v)
+		//fmt.Println(v.String())
 	}
 	if res, err = r.client.Do(req); err != nil {
 		return nil, err
 	}
+	
 	listCookie := res.Cookies()
+	
 	for _, v := range listCookie {
 		r._mapCookie[v.Name] = *v
 	}
+	//fmt.Println(res.StatusCode)
+	//fmt.Printf("%+v\n", r._mapCookie)
 	switch res.Header.Get("Content-Encoding") {
 	case "gzip":
 		reader, err = gzip.NewReader(res.Body)
@@ -121,4 +137,18 @@ func (r *RQ) Request(f func() (*http.Request, error)) ([]byte, error) {
 	}()
 	r._ref = req.URL.String()
 	return ioutil.ReadAll(reader)
+}
+
+func (r *RQ) ExportCookie() (list []*http.Cookie) {
+	list = make([]*http.Cookie, 0, len(r._mapCookie))
+	for _, v := range r._mapCookie {
+		nc := v
+		list = append(list, &nc)
+	}
+	return list
+}
+func (r *RQ) AddCookie(list []*http.Cookie) {
+	for _, v := range list {
+		r._mapCookie[v.Name] = *v
+	}
 }
